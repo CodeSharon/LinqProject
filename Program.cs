d6596f5474d5b6253334c4108fa2c2f7131d2e3a
Sharon Aline NGONO <86223765+CodeSharon@users.noreply.github.com>
2024-08-25 00:48:22 +0200

    removed comments

--- Program.cs
+++ Program.cs
@@ -8,16 +8,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Set the culture to ensure proper currency formatting
         CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 
         var carService = new CarService();
 
-        // Load data from both JSON and XML
         var carsFromJson = DataLoader.LoadFromJson("DataSource/cars.json");
         var carsFromXml = DataLoader.LoadFromXml("DataSource/cars.xml");
 
-        // Combine both data sources
         var combinedCars = carsFromJson.Concat(carsFromXml).ToList();
 
         Console.WriteLine("Welcome to the Car Finder, an easy way to find your dream car!");
@@ -32,7 +29,6 @@ class Program
 
         var availableCars = carService.FilterCarsByAge(combinedCars, userAge);
 
-        // If no cars are available, inform the user and exit
         if (availableCars.Count == 0)
         {
             Console.WriteLine("\nNo cars are available for your age.");
@@ -41,12 +37,10 @@ class Program
 
         carService.ShowAvailableCarMakes(availableCars);
 
-        // Enter a search term and filter based on text input
         Console.WriteLine("\nWhich model, year, price of car are you looking for?");
         string searchTerm = Console.ReadLine();
         var searchedCars = carService.SearchCars(availableCars, searchTerm);
 
-        // Sort cars by a specified criterion
         Console.WriteLine("\nEnter a sort criterion (price, year) or type 'skip' to skip:");
         string sortBy = Console.ReadLine();
 
